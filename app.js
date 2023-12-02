df4272385bfc66f59ac0a4941457d6dceecda0cf
Robert Allen <contact@robertdallen.com>
2023-12-02 15:52:15 -0500

    GA4 and About Author
    Google analytics and About author

--- app.js
+++ app.js
@@ -147,3 +147,17 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+// About the author
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutTrigger = document.getElementById('about-trigger');
+    if (aboutTrigger) {
+        aboutTrigger.onclick = function() {
+            var aboutBox = document.getElementById('about-box');
+            if (aboutBox) {
+                aboutBox.style.display = aboutBox.style.display === 'none' ? 'block' : 'none';
+            }
+        };
+    }
+  });
+  
